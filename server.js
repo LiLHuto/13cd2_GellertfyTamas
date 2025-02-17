67deb6bf2dbeb582e8373b022db998ebaff1354e
Gellértfy "Huto-GITANES" Tamás <82087132+LiLHuto@users.noreply.github.com>
2025-02-17 14:14:10 +0100

    müködik a szerver beállitása
    mysql szerver kapcsolatának beállitása létrehozása ellenörzö get megirása

--- server.js
+++ server.js
@@ -8,3 +8,18 @@ app.use((bodyparser));
 app.listen(3000,()=>(
     console.log("a szerver a 3000-es porton fut.")
 ))
+
+const db  = mysql.createConnection(
+    {
+        user: "root",
+        host: "localhost",
+        port:3307,
+        password:"",
+        database:"teliolimpia"
+    }
+)
+ //a szerver futásának ellenörzése// 
+ app.get("/",(req,res) => {
+    res.send("SzerverMüködik")
+ }
+)
